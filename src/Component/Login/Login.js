3a104acdce26e7e5549634dfa712dcb5c32d8def
Mehedi4467 <mehedi446768@gmail.com>
2022-05-19 06:12:21 +0600

    full complated log in and sign up design

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Spinner from '../Spinner/Spinner';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 
 const Login = () => {
+    const [resetEmail, setResetEmail] = useState('');
+    const [sendPasswordResetEmail, sending, emailError] = useSendPasswordResetEmail(
+        auth
+    );
     const [
         signInWithEmailAndPassword,
         user,
@@ -17,7 +23,6 @@ const Login = () => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password)
         signInWithEmailAndPassword(email, password);
 
     }
@@ -43,7 +48,7 @@ const Login = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                         Email
                     </label>
-                    <input name='email' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Your Email" required />
+                    <input onChange={(e) => setResetEmail(e.target.value)} name='email' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Your Email" required />
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -56,9 +61,17 @@ const Login = () => {
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Sign In
                     </button>
-                    <button className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" >
+                    <div onClick={async () => {
+                        await sendPasswordResetEmail(resetEmail);
+                        if (emailError) {
+                            toast(emailError.message)
+                        }
+                        else {
+                            toast("Send Email")
+                        }
+                    }} className="inline-block cursor-pointer align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" >
                         Forgot Password?
-                    </button>
+                    </div>
                 </div>
                 <div className="mt-2">
                     <h2 className='text-sm'>Don’t have an account? <Link className='text-orange-400' to='/signup'>Sign Up Now!</Link></h2>
